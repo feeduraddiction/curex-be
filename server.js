f7cb8fa2bfdc08f7ca58dabae42bcb13001e9da3
Ilya Charniatsou <81479127+feeduraddiction@users.noreply.github.com>
2021-12-22 23:21:05 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,20 +1,49 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
-app.use(cors())
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 const port = 3002;
 // import fetch from 'node-fetch';
 const fetch = require('node-fetch');
 // import fetch from 'node-fetch';
-const url = "https://www.nbrb.by/api/exrates/rates?periodicity=0";
-const settings = { method: "Get", base: "USD" };
-
+const url = "https://api.exchangerate.host/latest?base=USD&places=2&";
+const settings = { method: "Get"};
 fetch(url, settings)
     .then(res => res.json())
-    .then((json) => {
-        app.get('/', (req, res) => {
-            res.json(json);
+    .then((currencies) => {
+     const initialCurrencies = Object.fromEntries(
+       Object.entries(currencies.rates)
+     .filter(([key,value]) => 
+      (key === 'USD') || 
+      (key === 'EUR') ||
+      (key === 'BYN') ||
+      (key === 'RUB')
+      ))
+
+        app.get('/filtered', (req, res) => {
+            res.json(initialCurrencies);
           });
+
+        app.post('/filtered', (req, res) => {
+          console.log(req.body.key);
+          const currencyToAdd = req.body.key;
+         const filteredCurrencies = Object.assign(initialCurrencies, Object.fromEntries(
+           Object.entries(currencies.rates)
+           .filter(([key,value]) =>
+           key === currencyToAdd
+           )
+         ))
+          res.json(filteredCurrencies);
+        });
+
+        app.get('/', (req, res) => {
+          res.json(currencies.rates);
+        });
+          console.log(initialCurrencies);
+          console.log(currencies.rates);
     })
 
 app.listen(port, () => {
