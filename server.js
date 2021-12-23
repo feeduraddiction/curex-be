82edbbab61f67a980a58cfbb23cd9b0534c1af7e
feeduraddiction <alfanex62@gmail.com>
2021-12-24 00:06:50 +0300

    added logic for changing values

--- server.js
+++ server.js
@@ -6,45 +6,63 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const port = 3002;
-// import fetch from 'node-fetch';
 const fetch = require('node-fetch');
-// import fetch from 'node-fetch';
 const url = "https://api.exchangerate.host/latest?base=USD&places=2&";
-const settings = { method: "Get"};
+const settings = { method: "Get" };
+
+
 fetch(url, settings)
-    .then(res => res.json())
-    .then((currencies) => {
-     const initialCurrencies = Object.fromEntries(
-       Object.entries(currencies.rates)
-     .filter(([key,value]) => 
-      (key === 'USD') || 
-      (key === 'EUR') ||
-      (key === 'BYN') ||
-      (key === 'RUB')
+  .then(res => res.json())
+  .then((currencies) => {
+    const initialCurrencies = Object.fromEntries(
+      Object.entries(currencies.rates)
+        .filter(([key, value]) =>
+          (key === 'USD') ||
+          (key === 'EUR') ||
+          (key === 'BYN') ||
+          (key === 'RUB')
+        ))
+
+    const changeValueHandler = (req, res) => {
+      const inputValue = req.body.valueAfterChange === 0 ? 1 : req.body.valueAfterChange;
+      const focusedCurrency = req.body.currencyToChange;
+      const usdKoef = inputValue / initialCurrencies[focusedCurrency];
+      const newObject = Object.fromEntries(
+        Object.entries(initialCurrencies)
+          .map(([key, value]) => [key, value * usdKoef])
+      )
+      newObject.USD = usdKoef;
+      res.json(newObject);
+    }
+
+    const addCurrencyHandler = (req, res) => {
+      const currencyToAdd = req.body.key;
+      const currensciesToDisplay = Object.assign(initialCurrencies, Object.fromEntries(
+        Object.entries(currencies.rates)
+          .filter(([key, value]) =>
+            key === currencyToAdd
+          )
       ))
+      res.json(currensciesToDisplay);
+    }
 
-        app.get('/filtered', (req, res) => {
-            res.json(initialCurrencies);
-          });
+    app.get('/filtered', (req, res) => {
+      res.json(initialCurrencies);
+    });
 
-        app.post('/filtered', (req, res) => {
-          console.log(req.body.key);
-          const currencyToAdd = req.body.key;
-         const filteredCurrencies = Object.assign(initialCurrencies, Object.fromEntries(
-           Object.entries(currencies.rates)
-           .filter(([key,value]) =>
-           key === currencyToAdd
-           )
-         ))
-          res.json(filteredCurrencies);
-        });
+    app.post('/filtered', (req, res) => {
+      if (req.body.codeOfMethod === 1) {
+        addCurrencyHandler(req,res);
+      } else if (req.body.codeOfMethod === 2) {
+        changeValueHandler(req, res);
+      }
 
-        app.get('/', (req, res) => {
-          res.json(currencies.rates);
-        });
-          console.log(initialCurrencies);
-          console.log(currencies.rates);
-    })
+    });
+
+    app.get('/', (req, res) => {
+      res.json(currencies.rates);
+    });
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
